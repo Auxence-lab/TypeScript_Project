5886ae0594d1304f99507067c0104d9cb2c851b3
Auuxence <oxens2015@gmail.com>
2025-09-24 10:33:49 +0200

    Données Personnes et APi personnes remplies + template du dataset JSON

--- src/Personne.ts
+++ src/Personne.ts
@@ -1,3 +1,16 @@
 export interface Personne {
-
+    name : string;
+    birthCity : string;
+    birthState : string;
+    countryName : string;
+    countryCode2 : number;
+    countryCode3 : number;
+    LAT : number;
+    LON	: number;
+    birthyear : number;
+    gender : string;
+    occupation : string;
+    industry : string;
+    domain : string;
+    HPI : number;
 }
