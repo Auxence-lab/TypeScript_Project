c4f1429c77598eac4c24b180ca0f62da1debe906
Auuxence <oxens2015@gmail.com>
2025-09-27 21:55:12 +0200

    Ajout de code html pour la gae de base

--- src/Pantheon.controller.ts
+++ src/Pantheon.controller.ts
@@ -17,6 +17,42 @@ export class PantheonController {
     constructor(private readonly personneService: PantheonService) {
     }
 
+    @Get()
+    getHome(): string {
+        return `
+    <!DOCTYPE html>
+    <html lang="fr">
+    <head>
+      <meta charset="UTF-8">
+      <title>Pantheon API</title>
+      <style>
+        body { font-family: Arial, sans-serif; margin: 40px; }
+        h1 { color: #333; }
+        ul { list-style: none; padding: 0; }
+        li { margin: 10px 0; }
+        a { text-decoration: none; color: #007bff; }
+        a:hover { text-decoration: underline; }
+      </style>
+    </head>
+    <body>
+      <h1>Bienvenue sur l’API Pantheon</h1>
+      <h2>Cette APi a été crée par Lina et Auxence</h2>
+      <p>Voici les pages disponibles :</p>
+      <ul>
+        <li><a href="/personnes">Toutes les personnes</a></li>
+        <li><a href="/personnes/name">Recherche d'un personne avce le nom name</a></li>
+        <li><a href="/personnes?Page=1">Personnes paginées (Page 1)</a></li>
+        <li><a href="/search?term=Einstein">Recherche par terme</a></li>
+        <li><a href="/gender/male">Personnes de genre : masculin (male) féminin (female)</a></li>
+        <li><a href="/countrycode/FR">Personnes par countryCode</a></li>
+      </ul>
+      En vous souhaitant bonne navigation
+    </body>
+    </html>
+  `;
+    }
+
+
     @Post()
     createPersonne(@Body() personne: Personne): Personne {
         this.personneService.addPersonne(personne);
